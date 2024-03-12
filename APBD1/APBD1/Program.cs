c33cdefeac30cbd15060e275141e2b7dc07800e8
R3bl0 <mlaskowski512@gmail.com>
2024-03-12 22:12:09 +0100

    Mod 1

--- APBD1/APBD1/Program.cs
+++ APBD1/APBD1/Program.cs
@@ -2,3 +2,13 @@
 Console.WriteLine("Hello, User!");
 Console.WriteLine("Hello, User2");
 Console.WriteLine("Hello, User3");
+
+static double Avg(int[] liczby)
+{
+    double avg = 0;
+    foreach (int i in liczby)
+    {
+        avg += (double)liczby[i];
+    }
+    return avg / liczby.Length;
+}
